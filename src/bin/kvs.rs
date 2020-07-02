861c7036cd82ee082693f9b02fcd0dc6077d4336
Fernando Ledesma <frledesma@outlook.com>
2020-07-02 08:25:29 -0500

    finished part1 part2 part3 part4

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 use clap::{App, Arg, SubCommand};
 use std::process;
+use kvs::{KvStore, Result};
+use std::env::current_dir;
 
-fn main() {
+fn main() -> Result<()> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .subcommand(
@@ -25,16 +27,27 @@ fn main() {
 
     match matches.subcommand() {
         ("get", Some(_matches)) => {
-            eprintln!("unimplemented");
-            process::exit(1)
+            let key = _matches.value_of("KEY").expect("KEY argument missing");
+            let path = current_dir()?;
+            let mut store = KvStore::open(path)?;
+            let value_o = store.get(key.to_owned())?;
+            match value_o {
+                Some(v) => Ok(println!("value: {}", v)),
+                None => Ok(println!("value not present")),
+            }
         }
         ("set", Some(_matches)) => {
-            eprintln!("unimplemented");
-            process::exit(1)
+            let key = _matches.value_of("KEY").expect("KEY argument missing");
+            let value = _matches.value_of("VALUE").expect("VALUE argument missing");
+            let path = current_dir()?;
+            let mut store = KvStore::open(path)?;
+            store.set(key.to_owned(), value.to_owned())
         }
         ("rm", Some(_matches)) => {
-            eprintln!("unimplemented");
-            process::exit(1)
+            let key = _matches.value_of("KEY").expect("KEY argument missing");
+            let path = current_dir()?;
+            let mut store = KvStore::open(path)?;
+            store.remove(key.to_owned())
         }
         _ => unreachable!(),
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
+use std::fs::{OpenOptions, File};
+use std::fs;
+use std::result;
+use std::fmt;
+use std::io::Cursor;
+use std::io::{BufWriter, BufReader};
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::io::prelude::*;
 
 /// This is an example doc test
 ///
@@ -13,36 +23,116 @@ use std::collections::HashMap;
 /// let val = store.get("key".to_owned());
 /// assert_eq!(val, Some("value".to_owned()))
 /// ```
-#[derive(Default)]
+#[derive(Debug)]
 pub struct KvStore {
     map: HashMap<String, String>,
+    path: PathBuf,
+    writer: File,
 }
+
+#[derive(Debug)]
+pub enum KvStoreError{
+    SerdeIo(serde_json::Error),
+    Io(std::io::Error),
+    MapError(String)
+}
+pub type Result<T> = result::Result<T, KvStoreError>;
+
+impl From<std::io::Error> for KvStoreError {
+    fn from(err: std::io::Error) -> KvStoreError {
+        KvStoreError::Io(err)
+    }
+}
+
+impl fmt::Display for KvStoreError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "KvStore Error")
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+enum Command{
+    Get(String),
+    Set{key: String, value: String},
+    Rm(String),
+}
+
 
 impl KvStore {
     /// new method would create a new instance of `KvStore`
-    pub fn new() -> KvStore {
-        KvStore {
-            map: HashMap::new(),
-        }
+    // pub fn new() -> KvStore {
+    //     KvStore {
+    //         map: HashMap::new(),
+    //     }
+    // }
+
+    // pub fn open<P: AsRef<Path> + Sized>(path: P) -> Result<KvStore> {
+    fn deserialize_file(file: &File) -> HashMap<String, String>{
+        let mut map: HashMap<String, String> = HashMap::new();
+        let buf_reader = BufReader::new(file);
+        let mut de = serde_json::Deserializer::from_reader(buf_reader);
+        while let Ok(u) = Command::deserialize(&mut de) {
+            match u {
+                Command::Set{key, value} => map.insert(key, value),
+                Command::Rm(key) => map.remove(&key),
+                Command::Get(_) => Option::None,
+            };
+        };
+        return map;
+    }
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        let path = path.into();
+        let txt = path.join("cmd.json");
+        let file = OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(&txt)
+            .expect("Cannot open file");
+        let in_memory_ref = KvStore::deserialize_file(&file);
+        Ok(KvStore{
+            path: path.into(),
+            map: in_memory_ref,
+            writer: file,
+        })
     }
 
     /// assing a value to a specific key
     ///
     /// if the key already exists the value is overwritten
-    pub fn set(&mut self, key: String, value: String) {
-        self.map.insert(key, value);
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let cmd: Command = Command::Set{key: key.to_owned(), value: value.to_owned()};
+        serde_json::to_writer(&mut self.writer, &cmd)
+            .map_err(KvStoreError::SerdeIo)
+            .and_then(|_| {
+                self.map.insert(key, value);
+                Ok(())
+            })
     }
 
     /// gets the value of a specific key if there is some or none.
-    pub fn get(&mut self, key: String) -> Option<String> {
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        let cmd: Command = Command::Get(key.to_owned());
         match self.map.get(&key) {
-            Some(a) => Some(a.to_owned()),
-            None => None,
+            Some(a) => Ok(Some(a.to_owned())),
+            None => Ok(None),
         }
     }
 
     /// removes the the key and the associated value.
-    pub fn remove(&mut self, key: String) -> Option<String> {
-        self.map.remove(&key)
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        match self.map.contains_key(&key) {
+            true => {
+                let cmd: Command = Command::Rm(key.to_owned());
+                serde_json::to_writer(&mut self.writer, &cmd)
+                    .map_err(KvStoreError::SerdeIo)
+                    .and_then(|_| {
+                        self.map.remove(&key)
+                                .ok_or(KvStoreError::MapError("Unable to remove item".to_owned()))
+                                .and_then(|_| Ok(()))
+                    })
+            }
+            false => Ok(())
+        }
     }
 }
